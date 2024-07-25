8ea999eedc000a102957b46a718fb158bd476bf2
silvak <silvak.jeg@gmail.com>
2024-07-25 14:29:48 -0400

    change: remove text in navbar and btn stores in solutions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Suspense, lazy } from 'react';
-import Loader from './utils/Loader/Loader';
-import Layout from '../src/components/ui/Layout';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Footer from './Footer/Footer';
-import SliderComponent from './components/Sections/SliderComponent';
-import Solutions from './components/Sections/Solutions';
-import Demo from './components/Sections/Demo';
-import Resources from './components/Sections/Resources';
-import Testimonials from './components/Sections/Testimonials';
-import Posts from './components/Sections/Posts';
-import Project from './components/Sections/Project';
-import Contact from './components/Sections/Contact';
+import { Suspense, lazy } from "react";
+import Loader from "./utils/Loader/Loader";
+import Layout from "../src/components/ui/Layout";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Footer from "./Footer/Footer";
+import SliderComponent from "./components/Sections/SliderComponent";
+import Solutions from "./components/Sections/Solutions";
+import Demo from "./components/Sections/Demo";
+import Resources from "./components/Sections/Resources";
+import Testimonials from "./components/Sections/Testimonials";
+import Posts from "./components/Sections/Posts";
+import Project from "./components/Sections/Project";
+import Contact from "./components/Sections/Contact";
 
-const LazyProducts = lazy(() => import('./components/Sections/Products'));
+const LazyProducts = lazy(() => import("./components/Sections/Products"));
 
 function App() {
   return (
